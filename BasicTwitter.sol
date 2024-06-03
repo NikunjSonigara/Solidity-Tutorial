42b16de589c16fe572b2fe7e8ca1f2bebd68a58d
Nikunj Sonigara <106422654+NikunjSonigara@users.noreply.github.com>
2024-06-03 09:29:13 +0530

    Added Require

--- BasicTwitter.sol
+++ BasicTwitter.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.18;
 
 contract Twitter{
 
+    uint16 constant MAX_TWEET_LENGTH = 280;
+
     struct Tweet {
         address author;
         string content;
@@ -13,6 +15,9 @@ contract Twitter{
     mapping(address => Tweet[]) public tweets;
 
     function createTweet(string memory _tweet) public {
+
+        require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet is too long Bro!");
+
         Tweet memory newTweet = Tweet({
             author: msg.sender,
             content: _tweet,
@@ -23,8 +28,8 @@ contract Twitter{
         tweets[msg.sender].push(newTweet);
     }
 
-    function getTweet(address _owner, uint256 _index) public view returns (Tweet memory) {
-        return tweets[_owner][_index];
+    function getTweet(uint256 _index) public view returns (Tweet memory) {
+        return tweets[msg.sender][_index];
     }
 
     function getAllTweets(address _owner) public view returns (Tweet[] memory){
